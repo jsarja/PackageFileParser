f84b519fa4615a67696527da6994223d37a74482
Janne Sarja <jsarja1@gmail.com>
2019-11-11 22:08:35 +0200

    Create hbs templates to display the data in html format.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const path = require('path')
+const hbs = require('hbs');
+
 const PackageData = require('./PackageData')
 
 const app = express();
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+
 const port = process.env.PORT || 3000;
 let packageData = null;
+
+hbs.registerHelper('ifNotLast', function(v1, v2, options) {
+    if(v1 < v2-1) {
+        return options.fn(this);
+    }
+    return options.inverse(this);
+});
 
 app.get('/', (req, res) => {
     if(!packageData) {
@@ -11,23 +24,29 @@ app.get('/', (req, res) => {
             packageData = new PackageData();
         }
         catch (e) {
-            res.send('Error in reading the package file.');
+            return res.send('Error in reading the package file.');
         }
     }
-    res.send(packageData.getPackageList());
+
+    res.render('index', {packageList: packageData.getPackageList()});
 });
 
 app.get('/package/:packageName', (req, res) => {
     if(!packageData) {
         try {
+            console.log("Here")
             packageData = new PackageData();
+            console.log(packageData)
         }
         catch (e) {
             res.send('Error in reading the package file.');
         }
     }
 
-    res.send(packageData.getPackageInfo(req.params.packageName));
+    res.render(
+        'packageDetail', 
+        {packageInfo: packageData.getPackageInfo(req.params.packageName)}
+    );
 })
 
 app.listen(port, () => {
