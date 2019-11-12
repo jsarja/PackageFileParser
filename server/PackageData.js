3d2b7209ca1ada4912a2bf182b74c14c952f11fc
Janne Sarja <jsarja1@gmail.com>
2019-11-12 13:29:50 +0200

    Use depends information to create reverse mapping which creates list of packages that are depended on the current package.

--- server/PackageData.js
+++ server/PackageData.js
@@ -9,25 +9,25 @@ class PackageData {
     }
 
     _fetchPackageList() {
-        const fileBuffer = fs.readFileSync(process.env.PACKAGE_FILE_PATH)
+        const fileBuffer = fs.readFileSync(process.env.PACKAGE_FILE_PATH);
         const fileString = fileBuffer.toString();
-        const packageList = fileString.split('\n\n')
+        const packageList = fileString.split('\n\n');
 
         // Parse needed information from each package string.
         for(let i = 0; i < packageList.length-1; i++) {
-            const packageString = packageList[i]
-            this._fetchPackageDetails(packageString)
+            const packageString = packageList[i];
+            this._fetchPackageDetails(packageString);
         }
         this._packageNames.sort();
     }
 
     _fetchPackageDetails(str) {
         // Search package name.
-        let re = /Package:.*\n/
-        const name = str.match(re)[0].trim().replace('Package: ', '')
+        let re = /Package:.*\n/;
+        const name = str.match(re)[0].trim().replace('Package: ', '');
 
         // Search package description if it is provided.
-        re = /Description:[\s\S]*\n\S/
+        re = /Description:[\s\S]*\n\S/;
         let description = '';
         if(str.match(re)) {
             description = str.match(re)[0].slice(0, -1).trim()
@@ -35,7 +35,7 @@ class PackageData {
         }
 
         // Search package depedencies if they are provided.
-        re = /Depends:.*\n/
+        re = /Depends:.*\n/;
         let depends = [];
         if(str.match(re)) {
             // Also remove version information from inside the brackets.
@@ -43,15 +43,32 @@ class PackageData {
                 .replace('Depends: ', '').replace(/\([^\)]*\)/g,'');
             
             // Parse individual package names.
-            depends = dependsStr.split(',')
+            depends = dependsStr.split(',');
             depends = depends.map(pkg => {
-                // Some packages are separated with |.
+                // Some packages are separated with |. Save these packages as an
+                // array.
                 pkg = pkg.split(' | ');
-                return pkg.map(name => name.trim());
+                return pkg.map(depName => {
+                    const trimDepName = depName.trim();
+
+                    // Save the inverse depedency information also to the 
+                    // packages that the current package depends on.
+                    if(trimDepName in this._packageData) {
+                        this._packageData[trimDepName].provides.push(name);
+                    }
+                    else {
+                        this._packageData[trimDepName] = { provides: [name] };
+                    }
+                    return trimDepName;
+                });
             })
         }
 
-        this._packageData[name] = {name, description, depends, provides: []};
+        if (name in this._packageData) {
+            Object.assign(this._packageData[name], {name, description, depends});
+        } else {
+            this._packageData[name] = {name, description, depends, provides: []};
+        }
         this._packageNames.push(name);
     }
 
